059e94bfb411e3e12b71515bd036fb25822be1d5
omyrosnikova <lena.myroshnykova@gmail.com>
2021-04-14 14:50:33 +0200

    test test cu

---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/BaseTests.cs
@@ -0,0 +1,40 @@
+﻿using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost
+{
+    public class BaseTests
+    {
+        public static Partner CreateBasePartner(bool isActive = true)
+        {
+            var partner = new Partner()
+            {
+                Id = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8"),
+                Name = "Суперигрушки",
+                IsActive = isActive,
+                PartnerLimits = new List<PartnerPromoCodeLimit>()
+                {
+                    new PartnerPromoCodeLimit()
+                    {
+                        Id = Guid.Parse("e00633a5-978a-420e-a7d6-3e1dab116393"),
+                        CreateDate = new DateTime(2020, 07, 9),
+                        EndDate = new DateTime(2020, 10, 9),
+                        Limit = 100
+                    }
+                }
+            };
+
+            return partner;
+        }
+
+        public static SetPartnerPromoCodeLimitRequest GetTestPartnerPromoCodeLimitRequest(DateTime endDate, int limit = 2)
+          => new SetPartnerPromoCodeLimitRequest
+          {
+              EndDate = endDate,
+              Limit = limit
+          };
+    }
+}
--- src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -1,7 +1,61 @@
-﻿namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
     public class SetPartnerPromoCodeLimitAsyncTests
     {
-        //TODO: Add Unit Tests
+        private readonly PartnersController _partnersController;
+        private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+
+        public SetPartnerPromoCodeLimitAsyncTests()
+        {
+            _partnersRepositoryMock = new Mock<IRepository<Partner>>();
+            _partnersController = new PartnersController(_partnersRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task If_Partner_Not_Found_Should_Return_404Error()
+        {
+            // Arange
+            var limitRequest = BaseTests.GetTestPartnerPromoCodeLimitRequest(DateTime.Now);
+            
+            _partnersRepositoryMock.Setup(y => y.GetByIdAsync(It.IsAny<Guid>())).
+                ReturnsAsync(() => null);
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(Guid.NewGuid(), limitRequest) as NotFoundResult;
+
+            // Assert
+            Assert.Equal(result.StatusCode, 404);
+        }
+
+        [Fact]
+        public async Task If_Partner_Blocked_Should_Return_400Error()
+        {   
+            // Arange
+            var partnerId = Guid.NewGuid();
+            var limitRequest = BaseTests.GetTestPartnerPromoCodeLimitRequest(DateTime.Now);
+
+            _partnersRepositoryMock.Setup(y => y.GetByIdAsync(partnerId)).
+                ReturnsAsync(() => BaseTests.CreateBasePartner(false));
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(partnerId, limitRequest) as BadRequestObjectResult;
+
+            // Assert
+            Assert.Equal(result.StatusCode, 400);
+
+        }
+
+
+      
     }
 }
