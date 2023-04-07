01139a2d1f00b91168750a1df076a54a2178888f
Lesn Oleg <solar2010@live.ru>
2023-04-07 15:57:23 +0300

    Добавлены Partner, PartnerLimit, PartnerPromoCodeRequest builders

---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/Builders/PartnerBuilder.cs
@@ -0,0 +1,60 @@
+﻿using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using System;
+using System.Collections.Generic;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.Builders
+{
+    public class PartnerBuilder
+    {
+        private readonly Partner _partner;
+        public PartnerBuilder(Guid guid) 
+        {
+            _partner = new Partner()
+            {
+                Name = "СуперЗверушки",
+                IsActive = true,
+                NumberIssuedPromoCodes = 20,
+                PartnerLimits = new List<PartnerPromoCodeLimit>()
+                {
+                    new PartnerPromoCodeLimit()
+                    {
+                        Id = Guid.Parse("e00633a5-978a-420e-a7d6-3e1dab116393"),
+                        CreateDate = new DateTime(2022, 07, 9),
+                        EndDate = new DateTime(2023, 10, 9),
+                        Limit = 100,
+                    }
+                }
+            };
+        }
+
+        public PartnerBuilder IsActive(bool isActive = true)
+        {
+            _partner.IsActive = isActive;
+            return this;
+        }
+
+        public PartnerBuilder WithName(string name)
+        {
+            _partner.Name = name;
+            return this;
+        }
+
+        public PartnerBuilder WithPromoCodeLimits(PartnerPromoCodeLimit promocodeLimit)
+        {
+            promocodeLimit.PartnerId = _partner.Id;
+            _partner.PartnerLimits.Add(promocodeLimit);
+            return this;
+        }
+
+        public PartnerBuilder WithPromocodesCound(int numberIssuedPromocodes)
+        {
+            _partner.NumberIssuedPromoCodes = numberIssuedPromocodes;
+            return this;
+        }
+
+        public Partner Build()
+        {
+            return _partner;
+        }
+    }
+}
---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/Builders/PartnerLimitBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.Builders
+{
+    class PartnerLimitBuilder
+    {
+        private readonly PartnerPromoCodeLimit _partnerLimit;
+
+        public PartnerLimitBuilder()
+        {
+            _partnerLimit = new PartnerPromoCodeLimit()
+            {
+                Id = Guid.Parse("91a2846f-a9fb-443f-b3bd-30e621ab7986"),
+                CreateDate = DateTime.MinValue,
+                EndDate = DateTime.MaxValue,
+                CancelDate = null,
+                Limit = 10,
+            };
+
+        }
+
+        public PartnerLimitBuilder LimitedBy(int limit)
+        {
+            _partnerLimit.Limit = limit;
+            return this;
+        }
+
+        public PartnerLimitBuilder PartnerId(Guid id)
+        {
+            _partnerLimit.PartnerId = id;
+            return this;
+        }
+
+        public PartnerLimitBuilder CanceledAt(DateTime canceledAt)
+        {
+            _partnerLimit.CancelDate = canceledAt;
+            return this;
+        }
+
+        public PartnerPromoCodeLimit Build()
+        {
+            return _partnerLimit;
+        }
+
+
+
+    }
+}
