02e9e731dc8266dea339930aef38316095196456
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2023-07-21 17:45:48 +0300

    Добавлены тесты 1-2.

--- src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -1,7 +1,91 @@
-﻿namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.DataAccess;
+using Otus.Teaching.PromoCodeFactory.DataAccess.Data;
+using Otus.Teaching.PromoCodeFactory.DataAccess.Repositories;
+using Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
     public class SetPartnerPromoCodeLimitAsyncTests
     {
-        //TODO: Add Unit Tests
+        private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+        private readonly PartnersController _partnersController;
+
+        public SetPartnerPromoCodeLimitAsyncTests()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _partnersRepositoryMock = fixture.Freeze<Mock<IRepository<Partner>>>();
+            _partnersController = fixture.Build<PartnersController>().OmitAutoProperties().Create();
+        }
+
+        //Если партнер не найден, то также нужно выдать ошибку 404;
+        [Fact]
+        public async void TestPatnerNotFound()
+        {
+            Guid guid = Guid.Parse("def47943-7aaf-44a1-ae21-05aa4948b165");
+
+            Partner partner = null;
+            
+            _partnersRepositoryMock.Setup(repo => repo.GetByIdAsync(guid))
+               .ReturnsAsync(partner);
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(guid, default(SetPartnerPromoCodeLimitRequest));
+
+            // Assert
+            result.Should().BeAssignableTo<NotFoundResult>();
+        }
+
+        [Fact]
+        //Если партнер заблокирован, то есть поле IsActive=false в классе Partner, то также нужно выдать ошибку 400;
+        public async void TestPartnerIsBlocked()
+        {
+            Guid guid = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8");
+
+            Partner partner = this.CreateBasePartner();
+            partner.IsActive = false;
+
+            _partnersRepositoryMock.Setup(repo => repo.GetByIdAsync(guid))
+               .ReturnsAsync(partner);
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(guid, default(SetPartnerPromoCodeLimitRequest));
+
+            // Assert
+            result.Should().BeAssignableTo<BadRequestObjectResult>();
+        }
+        
+        public Partner CreateBasePartner()
+        {
+            var partner = new Partner()
+            {
+                Id = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8"),
+                Name = "Суперигрушки",
+                IsActive = true,
+                PartnerLimits = new List<PartnerPromoCodeLimit>()
+                {
+                    new PartnerPromoCodeLimit()
+                    {
+                        Id = Guid.Parse("e00633a5-978a-420e-a7d6-3e1dab116393"),
+                        CreateDate = new DateTime(2020, 07, 9),
+                        EndDate = new DateTime(2020, 10, 9),
+                        Limit = 100
+                    }
+                }
+            };
+
+            return partner;
+        }
     }
 }
