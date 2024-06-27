80578e07f30595c52836ddf04132dec57c67b282
Dofik77 <khorochavcev@mail.ru>
2024-06-27 18:00:00 +0300

    #feature/Homework-4 частичная реализация тестов

---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/Helpers/TestDataGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.Helpers;
+
+public class TestDataGenerator
+{
+    private readonly IFixture _fixture;
+    
+    public TestDataGenerator(IFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    public Guid GetRandomUuid() 
+        => Guid.NewGuid();
+
+    public SetPartnerPromoCodeLimitRequest GetSetPartnerPromoCodeLimitRequest() =>
+        _fixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+
+    public Partner GetPartner(bool? isActive, List<PartnerPromoCodeLimit> promoCodes = null) =>
+        _fixture
+            .Build<Partner>()
+            .With(x => x.IsActive, isActive)
+            .With(x => x.PartnerLimits, promoCodes)
+            .Create();
+
+    public PartnerPromoCodeLimit GetPartnerPromoCodeLimit(DateTime? limit = null) =>
+        _fixture
+            .Build<PartnerPromoCodeLimit>()
+            .With(x => x.CancelDate, limit)
+            .Create();
+}
